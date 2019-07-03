544690b8098f1291da17e63f50b5dbc2408ef78a
tinylife <37603388+tinylife@users.noreply.github.com>
2019-07-03 17:03:51 +0800

    Add files via upload

--- linemodLevelup/pybind11.cpp
+++ linemodLevelup/pybind11.cpp
@@ -2,9 +2,10 @@
 #include <pybind11/stl.h>
 #include "np2mat/ndarray_converter.h"
 #include "linemodLevelup.h"
+#include "pose_renderer.h"
 namespace py = pybind11;
 
-PYBIND11_MODULE(linemodLevelup_pybind, m) {
+PYBIND11_MODULE(patch_linemod_pybind, m) {
     NDArrayConverter::init_numpy();
 
     py::class_<poseRefine>(m, "poseRefine")
@@ -13,7 +14,17 @@ PYBIND11_MODULE(linemodLevelup_pybind, m) {
         .def_readwrite("inlier_rmse",&poseRefine::inlier_rmse)
         .def_readwrite("fitness",&poseRefine::fitness)
         .def("get_depth_edge", &poseRefine::get_depth_edge)
+        .def("set_depth", &poseRefine::set_depth, py::arg("depth"), py::arg("K") = cv::Mat())
         .def("process", &poseRefine::process);
+
+    py::class_<PoseRenderer>(m, "PoseRenderer")
+            .def(py::init<std::string, cv::Mat, cv::Mat>(), py::arg("model_path"),
+                 py::arg("depth") = cv::Mat(), py::arg("K") = cv::Mat())
+            .def("view_dep", &PoseRenderer::view_dep)
+            .def("set_K_width_height", &PoseRenderer::set_K_width_height)
+            .def("render_depth", &PoseRenderer::render_depth, py::arg("init_poses"), py::arg("down_sample") = 1)
+            .def("render_mask", &PoseRenderer::render_mask, py::arg("init_poses"), py::arg("down_sample") = 1)
+            .def("render_depth_mask", &PoseRenderer::render_depth_mask, py::arg("init_poses"), py::arg("down_sample") = 1);
 
     py::class_<linemodLevelup::Match>(m,"Match")
             .def(py::init<>())
@@ -34,10 +45,13 @@ PYBIND11_MODULE(linemodLevelup_pybind, m) {
         .def(py::init<>())
         .def(py::init<std::vector<int>, int>())
         .def(py::init<int, std::vector<int>, int>())
-        .def("addTemplate", &linemodLevelup::Detector::addTemplate)
+        .def("addTemplate", &linemodLevelup::Detector::addTemplate, py::arg("sources"), py::arg("class_id"),
+             py::arg("object_mask") = cv::Mat(), py::arg("dep_anchors") = std::vector<int>())
         .def("writeClasses", &linemodLevelup::Detector::writeClasses)
         .def("clear_classes", &linemodLevelup::Detector::clear_classes)
         .def("readClasses", &linemodLevelup::Detector::readClasses)
+        .def("write_matches", &linemodLevelup::Detector::write_matches)
+        .def("read_matches", &linemodLevelup::Detector::read_matches)
         .def("match", &linemodLevelup::Detector::match, py::arg("sources"),
              py::arg("threshold"), py::arg("active_ratio"), py::arg("class_ids"),
              py::arg("dep_anchors"), py::arg("dep_range"), py::arg("masks")=cv::Mat())
