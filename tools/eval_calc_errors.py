868a2fd3ab8d42de1ef7bf61d7e4483b3cc725f4
tinylife <37603388+tinylife@users.noreply.github.com>
2019-07-03 17:12:03 +0800

    Add files via upload

--- tools/eval_calc_errors.py
+++ tools/eval_calc_errors.py
@@ -16,7 +16,7 @@ from params.dataset_params import get_dataset_params
 # Results for which the errors will be calculated
 #-------------------------------------------------------------------------------
 
-#dataset = 'hinterstoisser'
+# dataset = 'hinterstoisser'
 # dataset = 'tless'
 # dataset = 'tudlight'
 # dataset = 'rutgers'
